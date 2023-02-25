c8479e753a50d6c67c10ae760903bfc9939408ac
Cruz Rocha <juancruzrocha@live.com>
2023-02-25 18:15:14 -0300

    PORT name properly
    PORT cannot be SERVER_PORT, must be PORT only

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const { conn } = require('./src/db.js');
 //   SERVER_PORT
 // } = process.env;
 
-const SERVER_PORT = process.env.SERVER_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(SERVER_PORT , () => {
-    console.log(`Listening at ${SERVER_PORT}`) // eslint-disable-line no-console
+  server.listen(PORT , () => {
+    console.log(`Listening at ${PORT}`) // eslint-disable-line no-console
   });
 } );
